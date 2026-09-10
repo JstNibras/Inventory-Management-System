7dc24c612497afae0ea0911bb97f3ef9f2765a8c
Ahmad Nibras CT <nibrasctkappil@gmail.com>
2026-09-10 17:33:53 +0530

    feat : created repositories

---
+++ src/repositories/IProductRepository.ts
@@ -0,0 +1,7 @@
+import { Product } from '../models/Product.js';
+
+export interface IProductRepository {
+  findAll(): Promise<Product[]>;
+  findById(id: string): Promise<Product | undefined>;
+  save(product: Product): Promise<void>;
+}
